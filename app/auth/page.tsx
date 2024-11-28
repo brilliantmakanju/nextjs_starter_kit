15e8fb04b356c450952c26caeca3597e840beaaa
Brilliant Makanju <brilliantmakanju4@gmail.com>
2024-11-28 10:29:13 -0500

    fix(dashboard): removed the dashboard page
    When building the pnpm found that dashboard has no layout file and throw an error

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -3,7 +3,7 @@
 import { AnimatePresence, motion } from 'framer-motion';
 import Image from 'next/image';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { FaGithub, FaGoogle } from 'react-icons/fa';
 
 import { Button } from '@/components/ui/button';
@@ -59,75 +59,77 @@ const AuthPage = () => {
 	};
 
 	return (
-		<div className="flex h-screen flex-col overflow-hidden bg-white text-[#4B5563] dark:bg-[#0A1930] dark:text-[#E5E7EB] md:flex-row">
-			<AnimatePresence initial={false} mode="wait">
-				{view === 'signup' ? (
-					<>
-						<motion.div
-							key="signup-form"
-							variants={slideVariants}
-							initial="enterLeft"
-							animate="center"
-							exit="exitLeft"
-							transition={{ duration: 0.5 }}
-							className="absolute bottom-0 left-0 top-0 z-10 flex w-full items-center justify-center bg-[#F0F4F8] dark:bg-[#1E3A8A] md:relative md:w-1/2"
-						>
-							<SignupForm setView={handleSetView} />
-						</motion.div>
-						<motion.div
-							key="signup-image"
-							className="relative hidden w-1/2 md:block"
-							initial={{ x: '100%' }}
-							animate={{ x: '0%' }}
-							exit={{ x: '100%' }}
-							transition={{ duration: 0.5 }}
-						>
-							<div className="absolute inset-0 bg-[#3B82F6] opacity-100 dark:bg-[#60A5FA]" />
-							<div className="absolute left-1/2 top-1/2 h-4 w-4 -translate-x-1/2 -translate-y-1/2 transform rounded-full bg-[#F97316]" />
-							<Image
-								src={'/Anime-Girl1.png'}
-								alt={'signup-image'}
-								fill
-								className={'object-cover'}
-							/>
-						</motion.div>
-					</>
-				) : (
-					<>
-						<motion.div
-							key="login-image"
-							className="relative hidden w-1/2 md:block"
-							initial={{ x: '-100%' }}
-							animate={{ x: '0%' }}
-							exit={{ x: '-100%' }}
-							transition={{ duration: 0.5 }}
-						>
-							<div className="absolute inset-0 bg-[#3B82F6] opacity-100 dark:bg-[#60A5FA]" />
-							<div className="absolute left-1/2 top-1/2 h-4 w-4 -translate-x-1/2 -translate-y-1/2 transform rounded-full bg-[#F97316]" />
-						</motion.div>
-						<motion.div
-							key="login-form"
-							variants={slideVariants}
-							initial="enterRight"
-							animate="center"
-							exit="exitRight"
-							transition={{ duration: 0.5 }}
-							className="absolute bottom-0 right-0 top-0 z-10 flex w-full items-center justify-center bg-[#F0F4F8] dark:bg-[#1E3A8A] md:relative md:w-1/2"
-						>
-							{view === 'login' && <LoginForm setView={handleSetView} />}
-							{view === 'forgot' && (
-								<ForgotPasswordForm setView={handleSetView} />
-							)}
-						</motion.div>
-					</>
-				)}
-			</AnimatePresence>
+		<Suspense fallback={<div>Loading</div>}>
+			<div className="flex h-screen flex-col overflow-hidden bg-white text-[#4B5563] dark:bg-[#0A1930] dark:text-[#E5E7EB] md:flex-row">
+				<AnimatePresence initial={false} mode="wait">
+					{view === 'signup' ? (
+						<>
+							<motion.div
+								key="signup-form"
+								variants={slideVariants}
+								initial="enterLeft"
+								animate="center"
+								exit="exitLeft"
+								transition={{ duration: 0.5 }}
+								className="absolute bottom-0 left-0 top-0 z-10 flex w-full items-center justify-center bg-[#F0F4F8] dark:bg-[#1E3A8A] md:relative md:w-1/2"
+							>
+								<SignupForm setView={handleSetView} />
+							</motion.div>
+							<motion.div
+								key="signup-image"
+								className="relative hidden w-1/2 md:block"
+								initial={{ x: '100%' }}
+								animate={{ x: '0%' }}
+								exit={{ x: '100%' }}
+								transition={{ duration: 0.5 }}
+							>
+								<div className="absolute inset-0 bg-[#3B82F6] opacity-100 dark:bg-[#60A5FA]" />
+								<div className="absolute left-1/2 top-1/2 h-4 w-4 -translate-x-1/2 -translate-y-1/2 transform rounded-full bg-[#F97316]" />
+								<Image
+									src={'/Anime-Girl1.png'}
+									alt={'signup-image'}
+									fill
+									className={'object-cover'}
+								/>
+							</motion.div>
+						</>
+					) : (
+						<>
+							<motion.div
+								key="login-image"
+								className="relative hidden w-1/2 md:block"
+								initial={{ x: '-100%' }}
+								animate={{ x: '0%' }}
+								exit={{ x: '-100%' }}
+								transition={{ duration: 0.5 }}
+							>
+								<div className="absolute inset-0 bg-[#3B82F6] opacity-100 dark:bg-[#60A5FA]" />
+								<div className="absolute left-1/2 top-1/2 h-4 w-4 -translate-x-1/2 -translate-y-1/2 transform rounded-full bg-[#F97316]" />
+							</motion.div>
+							<motion.div
+								key="login-form"
+								variants={slideVariants}
+								initial="enterRight"
+								animate="center"
+								exit="exitRight"
+								transition={{ duration: 0.5 }}
+								className="absolute bottom-0 right-0 top-0 z-10 flex w-full items-center justify-center bg-[#F0F4F8] dark:bg-[#1E3A8A] md:relative md:w-1/2"
+							>
+								{view === 'login' && <LoginForm setView={handleSetView} />}
+								{view === 'forgot' && (
+									<ForgotPasswordForm setView={handleSetView} />
+								)}
+							</motion.div>
+						</>
+					)}
+				</AnimatePresence>
 
-			{/* Mobile background image */}
-			<div className="fixed inset-0 z-0 md:hidden">
-				<div className="absolute inset-0 bg-[#3B82F6] opacity-70 dark:bg-[#60A5FA]" />
+				{/* Mobile background image */}
+				<div className="fixed inset-0 z-0 md:hidden">
+					<div className="absolute inset-0 bg-[#3B82F6] opacity-70 dark:bg-[#60A5FA]" />
+				</div>
 			</div>
-		</div>
+		</Suspense>
 	);
 };
 
